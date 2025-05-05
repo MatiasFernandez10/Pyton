7433b500079742aff9723a65a24bc9051703ed59
MatiasFernandez10 <matiasfernandezari@gmail.com>
2025-05-05 00:40:57 -0300

    Update eje11.py

--- eje11.py
+++ eje11.py
@@ -4,6 +4,9 @@ def eje1():
     salario = 0
 
     for n in range(0, auto):
-        
+        valores[n] = int(input("Ingrese el valor:"))
+        salario += valores[n]*0.05 + 200
+
+    print("el valor de cada auto es:", salario+5500)
 
     eje1()
